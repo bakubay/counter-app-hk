06e75a0b0882067a8e06af51fc26f50e079b3340
Gasssyr <grasbaku@gmail.com>
2023-11-11 23:50:41 +0800

    fix: commet prisma endpoint for now

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,30 +1,30 @@
-import { z } from "zod";
+// import { z } from "zod";
 
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+// import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
-export const postRouter = createTRPCRouter({
-  hello: publicProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input.text}`,
-      };
-    }),
+// export const postRouter = createTRPCRouter({
+//   hello: publicProcedure
+//     .input(z.object({ text: z.string() }))
+//     .query(({ input }) => {
+//       return {
+//         greeting: `Hello ${input.text}`,
+//       };
+//     }),
 
-  create: publicProcedure
-    .input(z.object({ id: z.string().min(1), value: z.string().min(1) }))
-    .mutation(async ({ ctx, input }) => {
-      return ctx.db.count.upsert({
-        create: {
-          // id: input.id,
-          // value: input.value
-        },
-      });
-    }),
+//   create: publicProcedure
+//     .input(z.object({ id: z.string().min(1), value: z.string().min(1) }))
+//     .mutation(async ({ ctx, input }) => {
+//       return ctx.db.count.upsert({
+//         create: {
+//           id: input.id,
+//           value: input.value
+//         },
+//       });
+//     }),
 
-  getLatest: publicProcedure.query(({ ctx }) => {
-    return ctx.db.counter.findFirst({
-      orderBy: { name: "asc" },
-    });
-  }),
-});
+//   getLatest: publicProcedure.query(({ ctx }) => {
+//     return ctx.db.counter.findFirst({
+//       orderBy: { name: "asc" },
+//     });
+//   }),
+// });
